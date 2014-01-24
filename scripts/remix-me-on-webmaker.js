2f34e34a1eeec3d3c010745c5b62cdb6619d0f54
Atul Varma <varmaa@gmail.com>
2014-01-24 13:52:02 -0500

    Revert "remix ribbons have URLs changed again."
    This reverts commit dbec793ae78c9b3cae0ff3e3ac3da4eb82541e44.

--- scripts/remix-me-on-webmaker.js
+++ scripts/remix-me-on-webmaker.js
@@ -1,7 +1,6 @@
 var $ = require('../lib/util').$;
 var baseURL = location.href.slice(0, -1);
 
-var WEBMAKER_RE = /^https:\/\/(thimble\.)?webmaker\.org/;
 var MAKE_RE = /^https:\/\/([A-Za-z0-9_\-]+).makes.org\/thimble\/([A-Za-z0-9_\-]+)_$/;
 
 require('./ribbonate');
@@ -17,9 +16,8 @@ if (MAKE_RE.test(location.href)) {
   }
 
   document.addEventListener("DOMContentLoaded", function() {
-    $("[data-ribbonate]").forEach(function(ribbon) {
-      if (WEBMAKER_RE.test(ribbon.getAttribute("href")))
-        ribbon.setAttribute("href", baseURL + "/remix");
+    $(".remix-ribbon").forEach(function(ribbon) {
+      ribbon.setAttribute("href", baseURL + "/remix");
     });
   }, false);
 }
