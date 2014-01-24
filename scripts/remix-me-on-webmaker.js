d3e685008697b102b8a9682d3e9f80e79e61de89
Atul Varma <varmaa@gmail.com>
2014-01-24 08:10:17 -0500

    remix ribbon now frame-busts and rewrites hrefs.

--- scripts/remix-me-on-webmaker.js
+++ scripts/remix-me-on-webmaker.js
@@ -1,8 +1,27 @@
+var $ = require('../lib/util').$;
 var style = document.createElement('style');
+var baseURL = location.href.slice(0, -1);
+
+var MAKE_RE = /^https:\/\/([A-Za-z0-9_\-]+).makes.org\/thimble\/([A-Za-z0-9_\-]+)_$/;
 
 style.textContent = require('../css/remix-me-on-webmaker.css');
 document.head.appendChild(style);
 
 require('./ribbonate');
 
-// TODO: replace href w/ remix link of current page if possible.
+if (MAKE_RE.test(location.href)) {
+  try {
+    if (location.href == top.location.href + '_') {
+      // Bust out of the iframe.
+      top.location.href = location.href;
+    }
+  } catch (e) {
+    if (window.console) window.console.error(e);
+  }
+
+  document.addEventListener("DOMContentLoaded", function() {
+    $(".remix-ribbon").forEach(function(ribbon) {
+      ribbon.setAttribute("href", baseURL + "/remix");
+    });
+  }, false);
+}
