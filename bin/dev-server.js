c9e454db8bb3875bdc0e53e3dbc8e6cb13d805ab
Atul Varma <varmaa@gmail.com>
2014-01-24 03:26:23 -0500

    bugfix dev-server.js.

--- bin/dev-server.js
+++ bin/dev-server.js
@@ -7,22 +7,23 @@ var PORT = process.env.PORT || 3000;
 
 var app = express();
 
-function buildJs(path) {
+function buildJs(path, res) {
   return browserify()
     .add('./scripts/' + path)
     .bundle({debug: true})
     .on('error', function(err) {
       console.error(err);
       res.send(err.toString()).end();
-    });
+    })
+    .pipe(res);
 }
 
 app.get('/ribbonate.js', function(req, res) {
-  return buildJs('auto-ribbonate.js').pipe(res);
+  return buildJs('auto-ribbonate.js', res);
 });
 
 app.get('/remix-me-on-webmaker.js', function(req, res) {
-  return buildJs('remix-me-on-webmaker.js').pipe(res);
+  return buildJs('remix-me-on-webmaker.js', res);
 });
 
 app.use(express.static(__dirname + '/..'));
