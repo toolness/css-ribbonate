d1ce812ea810c98f90542dbca4ffd93691c68861
Atul Varma <varmaa@gmail.com>
2014-01-24 14:08:06 -0500

    rebuild files

--- remix-me-on-webmaker.js
+++ remix-me-on-webmaker.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = ".remix-ribbon {\n  background: black;\n  color: white;\n  text-decoration: none;\n  padding: 10px;\n  font-family: 'Helvetica Neue', sans-serif;\n}\n\n.remix-ribbon:not(.remix-ribbon-ready) {\n  display: none;\n}\n";
+module.exports = ".remix-ribbon {\n  background: black;\n  color: white;\n  text-decoration: none;\n  padding: 10px;\n  font-family: 'Helvetica Neue', sans-serif;\n}\n\n.remix-ribbon:hover {\n  text-decoration: underline;\n}\n\n.remix-ribbon:not(.remix-ribbon-ready) {\n  display: none;\n}\n";
 },{}],2:[function(require,module,exports){
 var setVendorPrefix = require('./set-vendor-prefix');
 
