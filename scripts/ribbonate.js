ec3b0dad0aa35006d36b7693c55a12040070ea55
Atul Varma <varmaa@gmail.com>
2014-01-24 08:45:57 -0500

    change ribbonate.js to ribbonate on dom content load for now.

--- scripts/ribbonate.js
+++ scripts/ribbonate.js
@@ -1,7 +1,7 @@
 var ribbonate = require('../lib/ribbonate');
 var $ = require('../lib/util').$;
 
-window.addEventListener('load', function() {
+window.addEventListener('DOMContentLoaded', function() {
   $('[data-ribbonate]').forEach(function(el) {
     ribbonate(el, el.getAttribute('data-ribbonate'));
   });
